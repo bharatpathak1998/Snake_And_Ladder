972aec10ba7224c2ab62391c7ed28b62fdebc01c
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-09 21:38:25 +0530

    Added : Number Of Moves To Win

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,71 +1,45 @@
-package UC7;
+package UC6;
 
 import java.util.Random;
 
 public class SnakeAndLadder {
-    static void playerWin() {
+    static void numberOfMoves() {
         int winningPosition = 100;
-        int playerPosition1 = 0;
-        int playerPosition2 = 0;
+        int playerPosition = 0;
         int moves = 0;
         Random random = new Random();
 
-        while ((playerPosition1 != winningPosition) && (playerPosition2 != winningPosition)) {
+        while (playerPosition != winningPosition) {
             moves++;
 
-            int rollTheDice1 = random.nextInt(6) + 1;
-            int rollTheDice2 = random.nextInt(6) + 1;
-            System.out.println("Player1 after rolling the dice : " + rollTheDice1);
-            System.out.println("Player2 after rolling the dice : " + rollTheDice2);
+            int rollTheDice = random.nextInt(6)+1;
+            System.out.println("After rolling the dice : "+rollTheDice);
 
-            int optionCheck1 = random.nextInt(3);
-            int optionCheck2 = random.nextInt(3);
-            //Condition for first player
-            if (optionCheck1 == 0) {
-                System.out.println("Player1 No play : " + playerPosition1);
-            } else if (optionCheck1 == 1) {
-                if (playerPosition1 + rollTheDice1 > winningPosition) {
-                    playerPosition1 -= rollTheDice1;
-                    System.out.println("Player1 Ladder : " + playerPosition1);
-                } else {
-                    playerPosition1 += rollTheDice1;
-                    System.out.println("Player1 Ladder : " + playerPosition1);
-                }
-            } else {
-                playerPosition1 -= rollTheDice1;
-                if (playerPosition1 < 0)
-                    playerPosition1 = 0;
-                System.out.println("Player1 Snake : " + playerPosition1);
+            int optionCheck = random.nextInt(3);
+
+            if (optionCheck == 0) {
+                System.out.println("No play : "+playerPosition);
             }
-            //Condition for second player
-            if (optionCheck2 == 0) {
-                System.out.println("Player2 No play : " + playerPosition2);
-            } else if (optionCheck2 == 1) {
-                if (playerPosition2 + rollTheDice2 > winningPosition) {
-                    playerPosition2 -= rollTheDice2;
-                    System.out.println("Player2 Ladder : " + playerPosition2);
+            else if (optionCheck == 1) {
+                if (playerPosition+rollTheDice > winningPosition) {
+                    playerPosition -= rollTheDice;
+                    System.out.println("Ladder : "+playerPosition);
                 } else {
-                    playerPosition2 += rollTheDice2;
-                    System.out.println("Player2 Ladder : " + playerPosition2);
+                    playerPosition += rollTheDice;
+                    System.out.println("Ladder : "+playerPosition);
                 }
-            } else {
-                playerPosition2 -= rollTheDice2;
-                if (playerPosition2 < 0)
-                    playerPosition2 = 0;
-                System.out.println("Player2 Snake : " + playerPosition2);
+            }
+            else {
+                playerPosition -= rollTheDice;
+                if (playerPosition < 0)
+                    playerPosition = 0;
+                System.out.println("Snake : "+playerPosition);
             }
         }
-        System.out.println("Final position of player1 : " + playerPosition1);
-        System.out.println("Final position of player2 : " + playerPosition2);
         System.out.println("Number Of Total Moves : "+moves);
-
-        if(playerPosition1 > playerPosition2) {
-            System.out.println("Player1 Win The Game");
-        } else {
-            System.out.println("Player2 Win The Game");
-        }
     }
     public static void main(String[] args) {
-        playerWin();
+        numberOfMoves();
     }
+
 }

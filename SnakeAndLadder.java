ddcf1e667151c2fdd542187ca85f9d635dc1b6b9
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-09 18:47:34 +0530

    Added : Start Position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package UC1;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        System.out.println("Player start at Position : "+playerPosition);
+    }
+}

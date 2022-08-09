4f98ef8fee4b5c3026e41b64d0dc923f930adabc
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-09 19:06:12 +0530

    Added : To Exact Winning Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,15 +1,15 @@
-package UC4;
+package UC5;
 
 import java.util.Random;
 
 public class SnakeAndLadder {
-    static void winPosition() {
+    static void exactWin() {
         int winningPosition = 100;
         int playerPosition = 0;
+        Random random = new Random();
 
-        while (playerPosition < winningPosition) {
+        while (playerPosition != winningPosition) {
 
-            Random random = new Random();
             int rollTheDice = random.nextInt(6)+1;
             System.out.println("After rolling the dice : "+rollTheDice);
 
@@ -19,16 +19,23 @@ public class SnakeAndLadder {
                 System.out.println("No play : "+playerPosition);
             }
             else if (optionCheck == 1) {
-                playerPosition += rollTheDice;
-                System.out.println("Ladder : "+playerPosition);
+                if (playerPosition+rollTheDice > winningPosition) {
+                    playerPosition -= rollTheDice;
+                    System.out.println("Ladder : "+playerPosition);
+                } else {
+                    playerPosition += rollTheDice;
+                    System.out.println("Ladder : "+playerPosition);
+                }
             }
             else {
                 playerPosition -= rollTheDice;
+                if (playerPosition < 0)
+                    playerPosition = 0;
                 System.out.println("Snake : "+playerPosition);
             }
         }
     }
     public static void main(String[] args) {
-        winPosition();
+        exactWin();
     }
 }

2f2791b769dca81900274c8fc91ed69457ca28a2
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-09 18:54:27 +0530

    Added : Player Rolls The Dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,14 @@
-package UC1;
+package UC2;
+
+import java.util.Random;
 
 public class SnakeAndLadder {
+    static void rollTheDice() {
+        Random random = new Random();
+        int rollTheDice = random.nextInt(6)+1;
+        System.out.println("After rolling the dice : "+rollTheDice);
+    }
     public static void main(String[] args) {
-        int playerPosition = 0;
-        System.out.println("Player start at Position : "+playerPosition);
+        rollTheDice();
     }
 }
